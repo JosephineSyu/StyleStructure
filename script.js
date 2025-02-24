2a218d93f72719d87671e72c9b278a0ad7931855
JosephineSyu <syuj489@newschool.edu>
2025-02-24 05:15:40 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 const imageUrl = 'Images/Image.jpg';
+const characters = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.'];
+
+const img = new Image();
+img.src = imageUrl; 
+
+img.onload = function () {
+    const canvas = document.getElementById('imageCanvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+
+    const width = canvas.width; 
+    const height = canvas.height; 
+
+    let asciiImage = '';
+
+    for (let y = 0; y < height; y += 8) {
+        for (let x = 0; x < width; x += 8) {
+            const pixel = ctx.getImageData(x, y, 1, 1).data;
+            const r = pixel[0];
+            const g = pixel[1];
+            const b = pixel[2];
+
+            const brightness = (r + g +b ) / 3;
+
+            const charIndex = Math.floor((brightness / 255) * (characters.length-1));
+            asciiImage += characters[charIndex];
+        }
+        asciiImage += '\n';
+    }
+    document.getElementById('textOutput').textContent = asciiImage;
+    document.body.appendChild(img);
+    console.log("Success!");
+};
+    img.onerror = function () {
+        console.error("Error!");
+    };
